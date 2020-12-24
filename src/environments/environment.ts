f10818239def7ce138a2799bddca1974168556e9
gmanojisaac <gmanoj.isaac@gmail.com>
2020-12-24 11:25:41 +0530

    Firebase host

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAAUDYX9_WVaVJgMi1Gb_tALR0Tc6F1aXo",
-    authDomain: "projectdevelopmenttool.firebaseapp.com",
-    projectId: "projectdevelopmenttool",
-    storageBucket: "projectdevelopmenttool.appspot.com",
-    messagingSenderId: "155833140934",
-    appId: "1:155833140934:web:442f75f4d3cdd6bc7239c7",
-    measurementId: "G-B8502Q26SY"
+    apiKey: "AIzaSyBNyTuKkhkeiC9AqTonSgGXYbBys0fGfnc",
+    authDomain: "stepswise-9927f.firebaseapp.com",
+    projectId: "stepswise-9927f",
+    storageBucket: "stepswise-9927f.appspot.com",
+    messagingSenderId: "381078753301",
+    appId: "1:381078753301:web:57700794b0107550257e1c",
+    measurementId: "G-6Q0NJJJYBJ"
   }
 };
 
