bc0bddb948b0a918a8139bf2551cb3a231bfe8e5
merlinpratheesh <merlinpratheesh@gmail.com>
2021-01-11 14:22:26 +0530

    drag added

--- src/app/publicprojects/publicprojects.component.ts
+++ src/app/publicprojects/publicprojects.component.ts
@@ -1,17 +1,25 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { FormControl, Validators } from '@angular/forms';
 import firebase from 'firebase/app';
 import { BehaviorSubject, of, Subscription } from 'rxjs';
 import { map, switchMap, startWith, withLatestFrom } from 'rxjs/operators';
 import { projectControls, UserdataService, userProfile } from '../service/userdata.service';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import {ChangeDetectionStrategy} from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-publicprojects',
   templateUrl: './publicprojects.component.html',
-  styleUrls: ['./publicprojects.component.scss']
+  styleUrls: ['./publicprojects.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
+
 export class PublicprojectsComponent implements OnInit,AfterViewInit,OnDestroy {
+  @ViewChild(MatAccordion) accordion: MatAccordion;
+
   @Input() profileinfoUid: firebase.User;
   myprojectControls: projectControls = {
     publicprojectControl: new FormControl(null, Validators.required)
@@ -56,15 +64,19 @@ export class PublicprojectsComponent implements OnInit,AfterViewInit,OnDestroy {
         if (!publicProjectSelected || publicProjectSelected === '') {
           this.localpublicList = [];
           this.getPublicListSubscription?.unsubscribe();
-          this.publicList = this.getPublicList(this.db.doc(('/projectList/publicProjects')));          
+          this.publicList = this.getPublicList(this.db.doc(('/projectList/publicProjects')));    
+          return publicProjectSelected;
         }
       })).subscribe(_=>{
 
       });
   }
-
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.localpublicList, event.previousIndex, event.currentIndex);
+  }
   ngOnDestroy(){
     this.publicProjsel.unsubscribe();
     this.getPublicListSubscription?.unsubscribe();
   }
+
 }
