c583e31b2d19d233972ecc1d0e74d9a566fd6d26
merlinpratheesh <merlinpratheesh@gmail.com>
2021-01-20 15:05:43 +0530

    Error ID

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent} from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppSharedModule } from './app-shared/app-shared.module';
+
+import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { environment } from '../environments/environment';
-import { TreeStructureComponent } from './tree-structure/tree-structure.component';
-import { PublicprojectsComponent } from './publicprojects/publicprojects.component';
-import { PrivateprojectsComponent } from './privateprojects/privateprojects.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TreeStructureComponent,
-    PublicprojectsComponent,
-    PrivateprojectsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    AppSharedModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule, // firestore
-    AngularFireAuthModule, // auth
-    AngularFireStorageModule // storage
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/auth.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { BehaviorSubject } from 'rxjs';
+declare var window: any;
+import firebase from 'firebase/app'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user = new BehaviorSubject<any>(this.fireAuth.currentUser);
+  constructor(
+    private fireAuth: AngularFireAuth
+  ) {
+    this.init();
+  }
+
+  init(): void {
+    window.onload = () => {
+      window.google.accounts.id.initialize({
+        client_id: 1075525420003-393vor4371dr05rlou9890vf8hv8m7p4.apps.googleusercontent.com,
+        callback: (token) => {
+          this.handle(token);
+        }
+      });
+    };
+    this.fireAuth.onAuthStateChanged((user) => {
+      console.log('30',user);
+      this.user.next(user);
+      if (!user) {
+        console.log('30',user);
+        window.google.accounts.id.prompt();
+      }
+    });
+  }
+
+  handle(token): void {
+    const credential = (new (firebase.auth as any).GoogleAuthProvider());
+    this.fireAuth.signInWithCredential(credential);
+    console.log(this.handle(token));
+
+  }
+
+  signOut(): void {
+    this.fireAuth.signOut();
+  }
+}
